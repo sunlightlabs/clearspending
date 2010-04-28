db27dbe6fab7921b6c44b253e3a52696b1bdb1a8
kaitlin <kaitlin@kaitlin-desktop.(none)>
2010-04-28 14:21:10 -0400

    Finished up the scripts. Added a Consistency model in the metrics app. Also added an abstract model with fields common to all 3 metrics

--- metrics/models.py
+++ metrics/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 from cfda.models import Program, Agency
 
-class AgencyConsistency(models.Model):
 
+class Metric(models.Model):
+    
     fiscal_year = models.IntegerField(blank=False, null=False)
     agency = models.ForeignKey('cfda.Agency', blank=False, null=False)
+    
+    class Meta:
+        abstract = True
+
+class Consistency(Metric):
+
     total_cfda_obligations = models.DecimalField(max_digits=21, decimal_places=2, blank=False, null=False)
     total_usa_obligations =  models.DecimalField(max_digits=21, decimal_places=2, blank=False, null=False)
     total_programs =  models.IntegerField(blank=False, null=False)
@@ -16,6 +23,7 @@ class AgencyConsistency(models.Model):
     var_over_pct = models.DecimalField(max_digits=20, decimal_places=4, null=True) 
     std_under_pct = models.DecimalField(max_digits=20, decimal_places=4, null=True)
     std_over_pct = models.DecimalField(max_digits=20, decimal_places=4, null=True)
+
     
 
     
