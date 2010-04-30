36ced6d3afc1dea4997e97363f3a4d47d4d1c0a5
Thomas Lee <thomas.j.lee@gmail.com>
2010-04-29 22:33:37 -0400

    revisions -- direct from SQL

--- completeness/__init__.py
+++ completeness/__init__.py
@@ -3,6 +3,11 @@
 import sys, csv, os, imp, pickle
 from settings import *
 from completeness.statlib import stats
+from decimal import Decimal
+import MySQLdb
+
+BOOKMARK = 'completeness/bookmark.pickle'
+
 
 # convenience decorators for metrics
 
@@ -34,7 +39,9 @@ class Result(object):
         self.result_type = result_type
 
         self.tests_run = 0
-        self.tests_completed_without_error = 0
+        self.tests_completed_without_error = 0     
+        self.dollars_sum = 0
+        self.dollars_of_passed_tests = 0   
         self.sum = 0
         self.values = []
         
@@ -52,11 +59,16 @@ class Result(object):
         """ To be called after the successful completion of the metric/test """
         self.tests_completed_without_error += 1
         
-    def record_val(self, val):
+    def record_val(self, val, *args, **kwargs):
         """ To be passed the result of the metric/test """
+        if kwargs.has_key('dollars') and kwargs['dollars'] is not None:
+            self.dollars_sum += kwargs['dollars']
+
         if self.result_type is 'boolean':
             if val is True:
                 self.sum += 1
+                if kwargs.has_key('dollars') and kwargs['dollars'] is not None:
+                    self.dollars_of_passed_tests += kwargs['dollars']
         else:        
             self.sum += val
             self.values.append(val)
@@ -91,7 +103,6 @@ class MetricTester(object):
         for filename in os.listdir(str(''.join(__path__))):
             if filename[-3:]==".py" and not "__init__" in filename:
                 module_name = filename[:-3]
-                print filename, module_name
                 (p_file, p_pathname, p_description) = imp.find_module(module_name, [__name__])
                 if p_file is not None:                    
                     m = imp.load_module(module_name, p_file, p_pathname, p_description)
@@ -134,8 +145,13 @@ class MetricTester(object):
             
             success = True
             try:
+                dollars = None
+                try:
+                    dollars = Decimal(row['federal_funding_amount']) 
+                except Exception, e:
+                    raise e
                 self.results[cfda_number][metric_name].record_attempt()
-                self.results[cfda_number][metric_name].record_val(metric_func(row))
+                self.results[cfda_number][metric_name].record_val(metric_func(row), dollars=dollars)
                 self.results[cfda_number][metric_name].record_success()                
             except:
                 success = False
@@ -167,7 +183,7 @@ class MetricTester(object):
             pickle.dump(self.results, f)
             f.close()
 
-def main():
+def main_csv():
     """
     >>> from completeness.metrics import MetricTester
     >>> t = MetricTester()
@@ -189,3 +205,70 @@ def main():
         
     print mtester.emit()
         
+
+def _store_bookmark(offset):
+    f = open(BOOKMARK, 'w')
+    pickle.dump(offset, f)
+    f.close()
+
+
+def main_mysql():
+
+    mtester_all = MetricTester()
+    mtester = MetricTester()
+    
+    if not os.path.exists(BOOKMARK):
+        _store_bookmark(0)
+    
+    f = open(BOOKMARK, 'r')
+    offset = pickle.load(f)
+    f.close()
+    
+    conn = MySQLdb.connect(host=MYSQL_HOST, user=MYSQL_USER, passwd=MYSQL_PASSWORD, db=MYSQL_DATABASE)    
+    cursor = conn.cursor()
+
+    for year in FISCAL_YEARS:
+        
+        while True:
+            
+            print "querying - FY%d / offset %d" % (year, offset)
+            
+            sql = "SELECT %s FROM %s WHERE (fiscal_year=%d) AND record_id>%d ORDER BY record_id ASC LIMIT %d" % (','.join(CANONICAL_FIELD_ORDER), MYSQL_TABLE_NAME, year, offset, ROWS_PER_CYCLE)
+            print sql
+            cursor.execute(sql.replace("\n", " "))
+
+            print "finished query, processing..."
+
+            while True:
+                row = cursor.fetchone()
+                if row is None:
+                    break
+            
+                # convert to a hash for ease of use
+                row = row_to_dict(row)
+                mtester.run_metrics(row)        
+
+                offset = row['record_id']
+
+            # store intermediate results
+            f = open('output/%d/%d.pickle' % (year, offset), 'w')
+            f.write(mtester.emit())
+            f.close()
+            
+            # compact/reset tester objects
+            mtester_all.finish()
+            del mtester
+            mtester = MetricTester()
+                
+            # increment
+            offset += ROWS_PER_CYCLE
+            _store_bookmark(offset)
+
+
+        f = open('output/%d/all.pickle', 'w')
+        f.write(mtester_all.emit())
+        f.close()
+
+
+def main():
+    main_mysql()
