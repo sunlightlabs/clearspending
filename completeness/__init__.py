dd29b708c1534725e9279e9e2d9334625041bb2c
Thomas Lee <thomas.j.lee@gmail.com>
2010-04-30 18:06:19 -0400

    bunch of fixes to the testing chain -- hoping this works

--- completeness/__init__.py
+++ completeness/__init__.py
@@ -5,31 +5,10 @@ from settings import *
 from completeness.statlib import stats
 from decimal import Decimal
 import MySQLdb
+import doctest
 
 BOOKMARK = 'completeness/bookmark.pickle'
 
-
-# convenience decorators for metrics
-
-# true/false tests
-def boolean(target):
-    target.is_metric = True
-    target.metric_type = 'boolean'
-    return target
-        
-# calculates more aggregate statistics
-def real(target):
-    target.is_metric = True
-    target.metric_type = 'real'
-    return target
-    
-# calculates the same statistics as float, but also assembles a histogram
-def integer(target):
-    target.is_metric = True
-    target.metric_type = 'integer'
-    return target
-
-# /decorators
 
 class Result(object):
     """ Stores the results of our metric tests. A bit fancier than a dict. """
@@ -81,7 +60,8 @@ class Result(object):
     def finish(self):
         if self.result_type in ('integer', 'real'):
             self.mean = stats.mean(self.values)
-            self.std_dev = stats.stdev(self.values)
+            if len(self.values)>1:
+                self.std_dev = stats.stdev(self.values)
             self.count = len(self.values) # this should be the same as self.tests_completed_without_error, but is a little clearer for adding stats
         del self.values # no need to keep all that garbage
         
@@ -89,7 +69,11 @@ class Result(object):
 
 # main tester object
 class MetricTester(object):
-    """ Performs specified tests on rows """
+    """ Performs specified tests on rows 
+    >>> m = MetricTester()
+    ''
+    
+    """
     def __init__(self):
         super(MetricTester, self).__init__()
         
@@ -100,19 +84,16 @@ class MetricTester(object):
         
         # bootstrap tests
         self.metrics = {}
-        for filename in os.listdir(str(''.join(__path__))):
+        for filename in os.listdir('./completeness/metrics'):
             if filename[-3:]==".py" and not "__init__" in filename:
                 module_name = filename[:-3]
-                (p_file, p_pathname, p_description) = imp.find_module(module_name, [__name__])
-                if p_file is not None:                    
-                    m = imp.load_module(module_name, p_file, p_pathname, p_description)
-                    for candidate_name in dir(m):
-                        candidate = getattr(m, candidate_name)
-                        if callable(candidate):
-                            if getattr(candidate, 'is_metric', False):
-                                self.metrics["%s.%s" % (module_name, candidate_name)] = candidate
-    
-    
+                m = __import__('completeness.metrics.%s' % module_name)
+                for candidate_name in dir(m.metrics.metric_completeness):
+                    candidate = getattr(m.metrics.metric_completeness, candidate_name)
+                    if callable(candidate):
+                        if getattr(candidate, 'is_metric', False):
+                            self.metrics["%s.%s" % (module_name, candidate_name)] = candidate
+        
     def _row_to_dict(self, row):
         """ Turns the incoming row into a hash for ease of use """
         r = {}
@@ -129,13 +110,14 @@ class MetricTester(object):
         if len(row)!=len(CANONICAL_FIELD_ORDER):
             raise Exception("input row length (%d) does not match canonical row length (%d)" % (len(row), len(CANONICAL_FIELD_ORDER)))
 
-        # convert to a hash for convenience of the metric test functions
-        row = self._row_to_dict(row)
-
-        for (metric_name, metric_func) in self.metrics.items():
+        # if necessary, convert to a hash for convenience of the metric test functions
+        if type(row) is list:
+            row = self._row_to_dict(row)
+        
+        for (metric_name, metric_func) in self.metrics.items():            
             
             self.results['__all__'].record_attempt()            
-            
+
             # set up necessary hashes and result objects
             cfda_number = row['cfda_program_num']
             if not self.results.has_key(cfda_number):
@@ -144,17 +126,23 @@ class MetricTester(object):
                 self.results[cfda_number][metric_name] = Result(result_type=metric_func.metric_type)            
             
             success = True
+
+            # try:
+
+            dollars = None
             try:
-                dollars = None
-                try:
-                    dollars = Decimal(row['federal_funding_amount']) 
-                except Exception, e:
-                    raise e
-                self.results[cfda_number][metric_name].record_attempt()
-                self.results[cfda_number][metric_name].record_val(metric_func(row), dollars=dollars)
-                self.results[cfda_number][metric_name].record_success()                
-            except:
-                success = False
+                amt = len(str(row['fed_funding_amount']).strip())>0 and str(row['fed_funding_amount']) or '0'
+                dollars = Decimal(amt)
+            except Exception, e:
+                raise e
+            self.results[cfda_number][metric_name].record_attempt()
+            mf = metric_func(row)
+            self.results[cfda_number][metric_name].record_val(mf, dollars=dollars)
+            self.results[cfda_number][metric_name].record_success()             
+                               
+            # except Exception,e:
+            #     raise e
+            #     success = False
                 
             if success:
                 self.results['__all__'].record_success()
@@ -163,9 +151,9 @@ class MetricTester(object):
     def finish(self):
         """ calculate aggregate values """
         for cfda_program_num in self.results:
-            print self.results[cfda_program_num]
-            for metric in self.results[cfda_program_num]:
-                self.results[cfda_program_num][metric].finish()
+            if cfda_program_num!='__all__':
+                for metric in self.results[cfda_program_num].keys():
+                    self.results[cfda_program_num][metric].finish()
 
         self.results['__all__'].finish()
 
@@ -213,6 +201,65 @@ def _store_bookmark(offset):
     f.close()
 
 
+def main_debug():
+
+    RECORD_ID_INDEX = CANONICAL_FIELD_ORDER.index('record_id')
+
+    mtester_all = MetricTester()
+    mtester = MetricTester()
+
+    if not os.path.exists(BOOKMARK):
+        _store_bookmark(0)
+
+    f = open(BOOKMARK, 'r')
+    offset = pickle.load(f)
+    f.close()
+
+    conn = MySQLdb.connect(host=MYSQL_HOST, user=MYSQL_USER, passwd=MYSQL_PASSWORD, db=MYSQL_DATABASE, cursorclass=MySQLdb.cursors.DictCursor)    
+    cursor = conn.cursor()
+
+    for year in FISCAL_YEARS:
+
+        while True:
+
+            print "querying - FY%d / offset %d" % (year, offset)
+            # 
+            # sql = "SELECT * FROM %s WHERE (fiscal_year=%d) AND record_id>%d ORDER BY record_id ASC LIMIT %d" % (MYSQL_TABLE_NAME, year, offset, ROWS_PER_CYCLE)
+            # print sql
+            # cursor.execute(sql.replace("\n", " "))
+
+            print "finished query, processing..."
+
+            while True:
+                row = pickle.load(open('debug_row.pickle'))
+
+                # convert to a hash for ease of use
+                mtester.run_metrics(row)        
+                mtester_all.run_metrics(row)
+                offset = row['record_id']
+                
+                break
+
+            # store intermediate results
+            f = open('completeness/output/%d/%d.pickle' % (year, offset), 'w')
+            f.write(mtester.emit())
+            f.close()
+
+            # compact/reset tester objects
+            mtester_all.finish()
+            del mtester
+            mtester = MetricTester()
+
+            # increment
+            offset += ROWS_PER_CYCLE
+            _store_bookmark(offset)
+
+
+        f = open('output/%d/all.pickle', 'w')
+        f.write(mtester_all.emit())
+        f.close()
+
+
 def main_mysql():
 
     RECORD_ID_INDEX = CANONICAL_FIELD_ORDER.index('record_id')
@@ -227,7 +274,7 @@ def main_mysql():
     offset = pickle.load(f)
     f.close()
     
-    conn = MySQLdb.connect(host=MYSQL_HOST, user=MYSQL_USER, passwd=MYSQL_PASSWORD, db=MYSQL_DATABASE)    
+    conn = MySQLdb.connect(host=MYSQL_HOST, user=MYSQL_USER, passwd=MYSQL_PASSWORD, db=MYSQL_DATABASE, cursorclass=MySQLdb.cursors.DictCursor)    
     cursor = conn.cursor()
 
     for year in FISCAL_YEARS:
@@ -236,7 +283,7 @@ def main_mysql():
             
             print "querying - FY%d / offset %d" % (year, offset)
             
-            sql = "SELECT %s FROM %s WHERE (fiscal_year=%d) AND record_id>%d ORDER BY record_id ASC LIMIT %d" % (','.join(CANONICAL_FIELD_ORDER), MYSQL_TABLE_NAME, year, offset, ROWS_PER_CYCLE)
+            sql = "SELECT * FROM %s WHERE (fiscal_year=%d) AND record_id>%d ORDER BY record_id ASC LIMIT %d" % (MYSQL_TABLE_NAME, year, offset, ROWS_PER_CYCLE)
             print sql
             cursor.execute(sql.replace("\n", " "))
 
@@ -247,10 +294,12 @@ def main_mysql():
                 if row is None:
                     break
             
+                pickle.dump(row, open('debug_row.pickle', 'w'))
+            
                 # convert to a hash for ease of use
                 mtester.run_metrics(row)        
-
-                offset = row[RECORD_ID_INDEX]
+                mtester_all.run_metrics(row)
+                offset = row['record_id']
 
             # store intermediate results
             f = open('completeness/output/%d/%d.pickle' % (year, offset), 'w')
@@ -274,3 +323,8 @@ def main_mysql():
 
 def main():
     main_mysql()
+    # main_debug()
+
+if __name__ == '__main__':
+    if '--test' in sys.argv:
+        doctest.testmod()
