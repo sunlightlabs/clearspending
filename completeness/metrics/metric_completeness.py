d9ea1ab279f99439b761e10000ab189b5debe77c
Thomas Lee <thomas.j.lee@gmail.com>
2010-04-30 18:06:19 -0400

    bunch of fixes to the testing chain -- hoping this works

--- completeness/metrics/metric_completeness.py
+++ completeness/metrics/metric_completeness.py
@@ -1,7 +1,28 @@
 #!/bin/python
 
-from completeness.metrics import *
-import re
+import re, datetime
+
+# convenience decorators for metrics
+
+# true/false tests
+def boolean(target):
+    target.is_metric = True
+    target.metric_type = 'boolean'
+    return target
+        
+# calculates more aggregate statistics
+def real(target):
+    target.is_metric = True
+    target.metric_type = 'real'
+    return target
+    
+# calculates the same statistics as float, but also assembles a histogram
+def integer(target):
+    target.is_metric = True
+    target.metric_type = 'integer'
+    return target
+
+# /decorators
 
 re_non_nummeric = re.compile(r'[^\d]')
 re_good_date = re.compile(r'\d{4}\-\d{2}\-\d{2}')
@@ -27,7 +48,7 @@ def recipient_name_length(row):
     return len(row['recipient_name'].strip())
 
 @boolean
-def recipient_city_code_not_empty():
+def recipient_city_code_not_empty(row):
     return len(row['recipient_city_code'].strip()) > 0
     
 @boolean
@@ -61,7 +82,7 @@ def recipient_zip_code_length(row):
 
 @boolean
 def recipient_zip_code_is_numeric(row):
-    return re_non_nummeric.search(row['recipient_zip']) is None
+    return re_non_nummeric.search(str(row['recipient_zip'])) is None
 
 @boolean
 def recipient_type_is_not_empty(row):
@@ -89,19 +110,19 @@ def federal_award_id_is_not_empty(row):
 
 @boolean
 def federal_funding_amount_is_not_empty(row):
-    return len(row['fed_funding_amount'].strip())>0
+    return row['fed_funding_amount'] is not None and len(str(row['fed_funding_amount']).strip())>0
 
 @boolean
 def federal_funding_amount_is_numeric(row):
-    return re_non_nummeric.search(row['fed_funding_amount']) is None
+    return re_non_nummeric.search(str(row['fed_funding_amount'])) is None
 
 @boolean
 def obligation_action_date_is_not_empty(row):
-    return len(row['obligation_action_date'])>0
+    return (type(row['obligation_action_date']) is datetime.date) or len(str(row['obligation_action_date']))>0
 
 @boolean
 def obligation_action_date_is_properly_formatted(row):
-    return not (re_good_date.match(row['obligation_action_date']) is None)
+    return type(row['obligation_action_date'] is datetime.date) or (not (re_good_date.match(str(row['obligation_action_date'])) is None))
 
 @boolean
 def assistance_type_is_not_empty(row):
