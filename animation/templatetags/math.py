b02ef5b0c63f5ee232d96722d0c9f25251a7ddf0
Thomas Lee <thomas.j.lee@gmail.com>
2010-05-11 14:19:27 -0400

    animation stuff

---
+++ animation/templatetags/math.py
@@ -0,0 +1,23 @@
+from django import template
+
+register = template.Library()
+
+
+def mult(value, arg):
+    "Multiplies the arg and the value"
+    return int(value) * int(arg)
+mult.is_safe = False
+
+def sub(value, arg):
+    "Subtracts the arg from the value"
+    return int(value) - int(arg)
+sub.is_safe = False
+
+def div(value, arg):
+    "Divides the value by the arg"
+    return int(value) / int(arg)
+div.is_safe = False
+
+register.filter('mult', mult)
+register.filter('sub', sub)
+register.filter('div', div)
--- animation/views.py
+++ animation/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+import settings
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+
+
+def main(request):
+
+    return render_to_response('animation/map.html', {}, context_instance=RequestContext(request))
