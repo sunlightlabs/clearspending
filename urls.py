672062c0bc20d1f2110adf3d63b272e87b4ac13d
kaitlin <kaitlin@kaitlin-desktop.(none)>
2010-05-13 10:43:07 -0400

    added some urls to the config

--- urls.py
+++ urls.py
@@ -1,17 +1,20 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
+from django.views.generic.list_detail import object_list
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^faads_scorecard/', include('faads_scorecard.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     # Uncomment the next line to enable the admin:
     # (r'^admin/', include(admin.site.urls)),
+    (r'^scorecard/agency/(?P<agency_id>\d+)/', 'metrics.views.get_agency_detail'),
+    (r'^scorecard/program/(?P<program_id>\d+)/', 'metrics.views.get_program_detail'),
+    (r'^scorecard/$', 'metrics.views.scorecard'),
+    (r'^analysis/', 'direct_to_template', {'template': 'analysis.html'}),
+    (r'^methodology/', 'direct_to_template', {'template': 'methodology,html'}),
+    (r'^background/', 'direct_to_template', {'template': 'background.html'}),
+    (r'^corrections/', 'object_list', {'template': 'corrections.html'}),
+    (r'^$', 'direct_to_template', {'template':'index.html'})
 )
