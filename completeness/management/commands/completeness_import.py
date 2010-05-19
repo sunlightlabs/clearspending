75b619ddf4f1df46bf643d6eba93b53d1e94befd
Thomas Lee <thomas.j.lee@gmail.com>
2010-05-19 17:48:07 -0400

    adjusted import command to do percentages as dollar-weighted

--- completeness/management/commands/completeness_import.py
+++ completeness/management/commands/completeness_import.py
@@ -26,7 +26,8 @@ def main():
     for y in FISCAL_YEARS:
         print 'processing fiscal year %d' % y
         data_pct = load_pickle_file('completeness/output/%d.pickle' % y)
-        data_dollars = load_pickle_file('completeness/output/%d-dollars.pickle' % y)
+        data_bad_dollars = load_pickle_file('completeness/output/%d-dollars_misreported.pickle' % y)
+        data_total_dollars = load_pickle_file('completeness/output/%d-dollars_total.pickle' % y)
         
         for program_number in data_pct:      
             print '   processing %s' % program_number  
@@ -45,26 +46,15 @@ def main():
             
             total_successful_tests = 0
             total_tests = 0
+            
             for metric_name in data_pct[program_number]:
                 
                 if metric_name[0:2]=='__':
                     continue
                 
-                this_run_passed_tests = data_pct[program_number][metric_name].sum
-                this_run_total_tests = data_pct[program_number][metric_name].tests_completed_without_error
-                
-                total_successful_tests += this_run_passed_tests
-                total_tests += this_run_total_tests
-                
-                if this_run_passed_tests>this_run_total_tests:
-                    raise Exception('MISMATCH!!! %s %s %d %d' % (program_number, metric_name, this_run_passed_tests, this_run_total_tests))
-                
                 metric_model_name = metric_name.split('.')[-1]
-                setattr(PCD, '%s_dollars' % metric_model_name, Decimal(str(data_pct[program_number][metric_name].dollars_of_failed_tests)))
-                if this_run_total_tests>0:
-                    setattr(PCD, '%s_pct' % metric_model_name, Decimal(str(this_run_passed_tests / (1.0 * this_run_total_tests))))
-                else:
-                    setattr(PCD, '%s_pct' % metric_model_name, Decimal('0'))
+                setattr(PCD, metric_model_name, Decimal(str(data_pct[program_number][metric_name].dollars_of_failed_tests)))                
+
             PCD.save()
             
             # save program-level summary
@@ -72,11 +62,8 @@ def main():
             PC.fiscal_year = y
             PC.program = program
             PC.agency = PC.program.agency
-            if total_tests>0:
-                PC.completeness_pct = Decimal(str(total_successful_tests / (1.0 * total_tests)))
-            else:
-                PC.completeness_pct = Decimal('0')
-            PC.completeness_dollars = data_dollars.get(program.program_number, Decimal('0'))
+            PC.completeness_failed_dollars = data_bad_dollars.get(program.program_number, None)
+            PC.completeness_total_dollars = data_total_dollars.get(program.program_number, None)
             PC.save()
             
             
