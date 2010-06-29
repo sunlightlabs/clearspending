3d01e3c2f94f983d737e400cceaea1e1791f9b9b
kaitlin <kaitlin@kaitlin-desktop.(none)>
2010-06-29 16:18:09 -0400

    style changes, fixed up slide down on animation

--- media/scripts/clearspending.js
+++ media/scripts/clearspending.js
@@ -5,16 +5,8 @@ $().ready(function() {
        //$(this).addClass("active").parent().siblings("li").find("a").removeClass("active");
        $("#sidenav li a").removeClass("active");
        $(this).addClass("active");
-       $("#sidenav li p").hide("slow");
-       $(this).siblings("p").show("slow");
+       $("#sidenav li p").hide();
+       $(this).siblings("p").slideDown("fast").fadeIn("slow");;
        return false;
    });
-	$("a.expand").toggle(
-	  function(){
-	     $(this).addClass("expanded").siblings("p").slideDown("slow");
-	  },
-	  function(){
-  	     $(this).removeClass("expanded").siblings("p").slideUp("slow");
-  	  } 
-	);
 });
